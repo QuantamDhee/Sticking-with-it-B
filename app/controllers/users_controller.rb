c03554c8c576108d503d99049c16f86585134413
Johndhee <Johndheeballesteros13@gmail.com>
2021-06-22 13:59:07 -0700

    added user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+      end
+    
+      private 
+      
+      def show 
+        @user = User.find(params[:id])
+      end
 end
