f9ff0ac7be9677720e8100b3ae8bb0d02bbef48c
Johndhee <Johndheeballesteros13@gmail.com>
2021-06-24 20:01:11 -0700

    ?

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -3,22 +3,23 @@ class Api::V1::NotesController < ApplicationController
     before_action :find_note, only: [:show, :edit, :update, :destroy]
 
     def index 
-        @notes = Note.all
+        notes = Note.all
 
-        render json: @notes
+        render json: notes, only: [:id, :name, :description]
     end
 
     def show
-        render json: @note
+        note = Note.find_by(id: params[:id])
+        render json: {id: note.id, name: note.name, description: note.description } 
     end
 
     def create
 
-        category = Category.find_or_create_by(name: params[:category])
-        note = @note = Note.new(name: params[:name], description: params[:description], category: category)
-
-        if @note.save
-            render json: @note
+        # category = Category.find_or_create_by(name: params[:category])
+        # note = @note = Note.new(name: params[:name], description: params[:description], category: category)
+        note = Note.new(note_params)
+        if note.save
+            render json: note, status: :created
         else
             render json: {error: note.errors.full_messages}, status: :not_acceptable
         end
@@ -32,8 +33,8 @@ class Api::V1::NotesController < ApplicationController
     end
 
     def update 
-        if @note.update(params.require(:note).permit(:name, :description))
-           render json: @note
+        if note.update(params.require(:note).permit(:name, :description))
+           render json: note
         else
             render json: {error: note.errors.full_messages}, status: :not_acceptable
         end
@@ -43,14 +44,16 @@ class Api::V1::NotesController < ApplicationController
     private
 
     def find_note
-        @note = Note.find(params[:id])
+        note = Note.find_by(id: params[:id])
     end
 
     def note_params
-        category = Category.find_or_create_by(name: params[:category])
-        permitted = params.permit(:name, :description)
-        permitted["category_id"] = category.id
-        permitted
+        # category = Category.find_or_create_by(name: params[:category])
+        # permitted = params.permit(:name, :description)
+        # permitted["category_id"] = category.id
+        # permitted
+        params.require(:note).permit(:name, :description, :user_id, :category_id)
+
     end
 
 end
