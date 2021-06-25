3e09c6f4ace9a4da72b1dcad5398c6441cb89a43
Johndhee <Johndheeballesteros13@gmail.com>
2021-06-24 22:42:10 -0700

    working on delete

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -14,34 +14,22 @@ class Api::V1::NotesController < ApplicationController
 
     def create
 
-<<<<<<< HEAD
-        # category = Category.find_or_create_by(name: params[:category])
-        # note = @note = Note.new(name: params[:name], description: params[:description], category: category)
-        note = Note.new(note_params)
-        if note.save
-            render json: note, status: :created
-=======
         note = Note.new(note_params)
 
         if note.save
             render json: note
->>>>>>> d3bace5038c8f9020110c08bd99f3711a4def642
         else
             render json: {error: note.errors.full_messages}, status: :not_acceptable
         end
     end
 
     def destroy
-        
+        note = Note.find(params[:id])
     end
 
     def update 
-<<<<<<< HEAD
-        if note.update(params.require(:note).permit(:name, :description))
-=======
         note = Note.find(params[:id])
         if note.update(params.permit(:name, :description, :user_id, :category_id))
->>>>>>> d3bace5038c8f9020110c08bd99f3711a4def642
            render json: note
         else
             render json: {error: note.errors.full_messages}, status: :not_acceptable
@@ -51,24 +39,11 @@ class Api::V1::NotesController < ApplicationController
     private
 
     def find_note
-<<<<<<< HEAD
-        note = Note.find_by(id: params[:id])
-    end
-
-    def note_params
-        # category = Category.find_or_create_by(name: params[:category])
-        # permitted = params.permit(:name, :description)
-        # permitted["category_id"] = category.id
-        # permitted
-        params.require(:note).permit(:name, :description, :user_id, :category_id)
-
-=======
         note = Note.find(params[:id])
     end
 
     def note_params
         params.permit(:name, :description, :user_id, :category_id)
->>>>>>> d3bace5038c8f9020110c08bd99f3711a4def642
     end
 
 end
